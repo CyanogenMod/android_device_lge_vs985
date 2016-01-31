1a859ff8466d7c9ca084defa21296c4dbd772f21
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-01-31 17:19:52 -0600

    vs985: support 46a baseband and update fingerprint
    Change-Id: I7833725bf029f0dc447ac3b82cfdcbd21bba81b9

--- cm.mk
+++ cm.mk
@@ -11,7 +11,7 @@ PRODUCT_NAME := cm_vs985
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_vzw_us" \
-    BUILD_FINGERPRINT="lge/g3_vzw/g3:5.1.1/LMY47V/1525410464a1b:user/release-keys" \
-    PRIVATE_BUILD_DESC="g3_vzw-user 5.1.1 LMY47V 1525410464a1b release-keys"
+    BUILD_FINGERPRINT="lge/g3_vzw/g3:6.0/MRA58K/160141503d43c:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_vzw-user 6.0 MRA58K 160141503d43c release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-verizon
